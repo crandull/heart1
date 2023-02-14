a3465011f9e899de2cd84ff541248d9e3a7a570b
crandull <116753458+crandull@users.noreply.github.com>
2023-02-14 15:23:20 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,3 @@
+music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+basic.showIcon(IconNames.Heart)
 basic.showIcon(IconNames.Heart)
